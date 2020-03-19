bbdcddda054d5273da447564c4475cde28224418
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-19 18:29:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,9 +20,11 @@ def find_the_chees(array)# code an argument here
   # the array below is here to help
   #cheese_types = ["cheddar", "gouda", "camembert"]
 
-   if array.indluce?("cheddar" || "gouda")
-     return "chedder" || "gouda"
-   else return nil
+   if array.indluce?("cheddar")
+     return "cheddar"
+   elsif  array.include?("gouda")
+     return "gouda"
+   else return nil 
      end
 
 end
