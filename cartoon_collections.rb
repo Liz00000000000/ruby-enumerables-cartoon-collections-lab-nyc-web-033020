28f543ad3a9b820f39eb3c0fb7d50a0e3ed19aec
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-19 18:15:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+array.each_with_index{ |item, index| puts "#{index + 1}. #{item}" }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map { |e| e.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  if array.find{ |word| word.size > 4}
+    return true
+  else return false
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_chees(array)# code an argument here
   # the array below is here to help
+
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
