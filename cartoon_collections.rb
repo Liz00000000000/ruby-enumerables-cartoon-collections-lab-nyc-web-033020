81d0a4d28afaffb886bd3bf69683dadeffcbeb32
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-19 18:25:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 
 def find_the_chees(array)# code an argument here
   # the array below is here to help
-  #cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types = ["cheddar", "gouda", "camembert"]
 
    if array.indluce?("cheddar")
        return "cheddar"
